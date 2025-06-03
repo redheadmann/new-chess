4c253872552dafe69922c7c9f854665c07a95840
Henry <henrykfetzer@gmail.com>
2025-06-03 01:52:59 -0400

    Bones complete for phase 5
    Most things will just tell you that they are not implemented, but I think I finally have the right setup.

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,4 +1,5 @@
 import chess.*;
+import ui.Repl;
 
 public class Main {
     public static void main(String[] args) {
--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -22,7 +22,7 @@ public class Repl {
         System.out.print(postLoginClient.help());
 
         Scanner scanner = new Scanner(System.in);
-        var result = "";
+        String result = "";
         while (!result.equals("quit")) {
             printPrompt();
             String line = scanner.nextLine();
@@ -30,11 +30,11 @@ public class Repl {
             try {
                 switch (state) {
                     case SIGNED_IN -> {
-                        result = PostLoginClient.eval(line);
+                        result = postLoginClient.eval(line);
                         System.out.print(SET_TEXT_COLOR_BLUE + result);
                     }
                     case SIGNED_OUT -> {
-                        result = PreLoginClient.eval(line);
+                        result = preLoginClient.eval(line);
                         System.out.print(SET_TEXT_COLOR_BLUE + result);
                     }
                     case IN_GAME -> {
@@ -51,10 +51,12 @@ public class Repl {
         System.out.println();
     }
 
+    /*
     public void notify(Notification notification) {
         System.out.println(RED + notification.message());
         printPrompt();
     }
+     */
 
     private void printPrompt() {
         String stateString = switch (state) {
